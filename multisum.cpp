6b9338987d8511bdba77ded7b5ef96786cb87349
Rajdeep0511 <palr73414@gmail.com>
2024-09-29 17:59:05 +0530

    Create multisum.cpp

---
+++ multisum.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int A(int n,int m)
+{
+  if(n==0)
+  {
+    cout<<'a'<<m; 
+    return 1;
+  }
+  else{
+    cout<<"a"<<m-n<<" + x(";
+    return A(n-1,m);
+  }
+}
+
+int main()
+{
+  int n,m;
+  cout<<"enter the power of x ";
+  cin>>n;
+  m=n;
+  A(n,m);
+  for(int i=0; i<m;i++){
+    cout<<")";
+  }
+return 0; 
+}
