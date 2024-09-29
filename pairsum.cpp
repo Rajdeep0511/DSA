5b88e96340440438d2e723d5c24a57a97d4f4c03
Rajdeep0511 <palr73414@gmail.com>
2024-09-29 17:55:32 +0530

    Create pairsum.cpp

---
+++ pairsum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4,5,7};
+    int k;
+    for(int i=0;i<6;i++)
+    {
+        for(int j=0;j<6;j++)
+        {
+            
+
+        }
+    }
+
+
+}
