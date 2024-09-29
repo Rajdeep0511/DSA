6b2328894442abb46a44e0a7968ae8394dc33973
Rajdeep0511 <palr73414@gmail.com>
2024-09-30 01:27:00 +0530

    Update pairsum.cpp

--- pairsum.cpp
+++ pairsum.cpp
@@ -1,16 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int arr[]={1,2,3,4,5,7};
-    int k;
-    for(int i=0;i<6;i++)
-    {
-        for(int j=0;j<6;j++)
-        {
-            
 
+void findPairWithSum(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == target) {
+                cout << "Pair found: (" << arr[i] << ", " << arr[j] << ")" << endl;
+                return;
+            }
         }
     }
+    cout << "No pair found with the given sum." << endl;
+}
 
+int main() {
+    int arr[] = {8, 7, 2, 5, 3, 1};
+    int target = 10;
+    int n = sizeof(arr) / sizeof(arr[0]);
 
+    findPairWithSum(arr, n, target);
+
+    return 0;
 }
